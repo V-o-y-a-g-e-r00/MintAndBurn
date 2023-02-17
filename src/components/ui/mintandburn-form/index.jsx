d7e3efacb77802ae56ca04e9288865e186526161
fmoloshnikov <fmoloshnikov@gmail.com>
2023-02-17 19:07:56 +0300

    делаю

--- src/components/ui/mintandburn-form/index.jsx
+++ src/components/ui/mintandburn-form/index.jsx
@@ -6,9 +6,63 @@ const MintandburnForm = () => {
         <section className={classes.testSection}>
             <div className={classes.mintandburnForm}>
                 <div className="header">
-                    header
+                    <div className="icon-holder">
+                        icon
+                    </div>
+                    <div className="gap">
+
+                    </div>
+                    <div className="text-holder margin-right">
+                        Mint Gcoin
+                    </div>
                 </div>
-                test
+                <div className='amount-container'>
+                    <div className='right-size-container'>
+                        Balance: 0
+                    </div>
+                    <div className='input-container'>
+                        <input type="text" />
+                        <div className='max-amount-container'>
+                            Max
+                        </div>
+                    </div>
+                    <div className='right-size-container'>
+                        Min amount: 0
+                    </div>
+                </div>
+                <div className='details-list'>
+
+
+                    <div className='details-item'>
+                        <div className='property-name'>
+                            You will pay
+                        </div>
+                        <div className='property-value'>
+                            N/A
+                        </div>
+                    </div>
+                    <div className='details-item'>
+                        <div className='property-name'>
+                            You will pay
+                        </div>
+                        <div className='property-value'>
+                            N/A
+                        </div>
+                    </div>
+                    <div className='details-item'>
+                        <div className='property-name'>
+                            You will pay
+                        </div>
+                        <div className='property-value'>
+                            N/A
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+                <div className='button-component'>Mint</div>
             </div>
         </section>
     )
--- src/components/ui/mintandburn-form/styles.js
+++ src/components/ui/mintandburn-form/styles.js
@@ -12,15 +12,93 @@ export const useStyles = makeStyles({
     },
 
     mintandburnForm: {
+        '--base-font-color': '#FFFFFF',
+        '--form-header-font-size': '2.1875rem',
+        '--form-content-font-size': '1.25rem',
+    
         width: '100%',
         height: '46.625rem',
-
-        padding: '3.125rem',
         background: 'blue',
         'border-radius': '2.5rem',
-        '& .header' : {
-            color: 'red'
+        'color': 'var(--base-font-color)',
+        'font-size': 'var(--form-content-font-size)',
 
+        padding: '3.125rem',
+        display: 'flex',
+        'flex-direction': 'column', 
+        
+
+        '& .header': {
+            // color: 'var(--color)',
+            width: '100%',
+            'margin-bottom': '2.3125rem',
+
+            display: 'flex',
+            'align-items': 'center',
+            
+
+            '& .icon-holder': {
+                'width':'4.0625rem',
+                'height': '3.9375rem',
+
+                'border': '1px solid red',
+                'flex-shrink': '0',
+            },
+            '& .gap':{
+                'flex':'0 1 1.5rem',
+            },
+            '& .text-holder':{
+                'font-size':'var(--form-header-font-size)',
+            }
+        },
+        '& .amount-container':{
+            'margin-bottom': '2.5rem',
+
+            '& .right-size-container':{
+                'margin':'auto',
+                'display': 'flex',
+                'justify-content': 'end',
+            },
+            '& .input-container':{
+                'position': 'relative',
+                'margin': '0.9375rem 0px',
+
+                '& input':{
+                    '-webkit-appearance': 'none',
+                    'outline': 'none',
+                    'border': 'none',
+                    'min-width': 'unset',
+                    'width': '100%',
+                    'padding': '1.4375rem 0.9375rem',
+
+                    'border-radius': '0.9375rem',
+                    'font-size': 'var(--form-content-font-size)',
+                    
+                },
+                '& .max-amount-container':{
+                    'position':'absolute',
+                    'right': '0.625rem',
+                    'top': '0px',
+                    'height': '100%',
+
+                    'display': 'flex',
+                    'align-items': 'center',
+                }
+            }
+        },
+        '& .details-list':{
+            'margin-bottom': '3.75rem',
+
+
+            '& .details-item':{
+                'margin-bottom':'2.8125rem',
+                'display': 'flex',
+                'justify-content': 'space-between',
+
+                '&:last-child':{
+                    'margin-bottom': '0px',
+                }
+            },
         }
     }
 
